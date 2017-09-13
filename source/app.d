1d6d398c68e3155f68c30c85b8421ee89d176bea
Orfeo Da Vià <orfeo.davia@microline.eu>
2017-09-13 11:53:58 +0200

    Add array of struct

--- source/app.d
+++ source/app.d
@@ -2,32 +2,85 @@ module app;
 
 import vibe.d;
 
-class SampleService {
-	// "GET /"
-	@path("/") void getHome() {
-		render!("home.dt");
-	}
+struct Data {
+   string name;
+   int age;
+}
+struct Doc {
+   string name;
+   int size;
+}
+struct Dir {
+   string name;
+   Doc[] docs;
+}
 
-	// POST /settings
-	void postSettings(string[] x) {
-	/*void postSettings(bool[] x) {*/
+
+class SampleService {
+   // get /settings
+   void getSettings() {
+      logInfo("GET SET");
+      import std.string;
+      string[] x;
+      foreach (i; 0 .. 3) {
+         x ~= "%s".format(i);
+      }
+      render!("settings.dt", x);
+   }
+
+   // POST /settings
+   void postSettings(string[] x) {
+      logInfo("POST SET %s", x);
       foreach (i, v; x) {
          logInfo("x[%s]=%s", i, v);
       }
-      redirect("./");
-	}
+      redirect("./settings");
+   }
+// get /conf
+   void getConf() {
+      logInfo("GET conf");
+      import std.string;
+      Data conf0 = Data("o", 53);
+      Data conf1 = Data("r", 13);
+      Data[] conf = [conf0, conf1];
+
+      render!("conf.dt", conf);
+   }
+
+   // POST /conf
+   void postConf(Data[] conf) {
+      logInfo("POST conf %s", conf);
+      redirect("./conf");
+   }
+
+   // get /dir
+   void getDir() {
+      logInfo("GET dir");
+      import std.string;
+      Dir dir = Dir("prova");
+      dir.docs ~= Doc("aa", 16);
+      dir.docs ~= Doc("bb", 32);
+      render!("dir.dt", dir);
+   }
+
+   // POST /dir
+   void postDir(Dir dir) {
+      logInfo("POST dir %s", dir);
+      redirect("./dir");
+   }
+
 }
 
 shared static this() {
-	auto router = new URLRouter;
-	router.registerWebInterface(new SampleService);
-	router.get("*", serveStaticFiles("public/"));
+   auto router = new URLRouter;
+   router.registerWebInterface(new SampleService);
+   router.get("*", serveStaticFiles("public/"));
 
-	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
+   auto settings = new HTTPServerSettings;
+   settings.port = 8080;
+   settings.bindAddresses = ["::1", "127.0.0.1"];
    settings.accessLogToConsole = true;
-	listenHTTP(settings, router);
+   listenHTTP(settings, router);
 
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+   logInfo("Please open http://127.0.0.1:8080/ in your browser.");
 }
